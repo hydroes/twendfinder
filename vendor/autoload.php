898c28b074d455c5802829da1b8ba5ced3e08543
Brian Gouws - work <briangouws@gmail.com>
2013-11-22 13:50:18 +0000

    regenerated autoload

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit27b2baf179f0e52f7d08176cd42e3e50::getLoader();
+return ComposerAutoloaderInite041743da033b4248df6b49669be992e::getLoader();
