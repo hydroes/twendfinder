bf5e2de2807609a02c762557f41bf5d026676d4a
Brian Gouws - work <briangouws@gmail.com>
2013-11-22 14:20:24 +0000

    queue testing

--- app/library/App/Queues/QueueTwitterStatus.php
+++ app/library/App/Queues/QueueTwitterStatus.php
@@ -8,8 +8,8 @@ class QueueTwitterStatus
 
 //        var_dump('Job ID: '.$job->getJobId());
 //        var_dump($data);
-        Log::info('This is some useful information about job: ' . $job->getJobId());
-        sleep(60);
+//        Log::info('This is some useful information about job: ' . $job->getJobId());
+//        sleep(60);
         // NB: be sure to release job else it never leaves
         $job->delete();
     }
