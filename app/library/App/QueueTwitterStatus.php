b7d35aa99ab2d49786bee054e9be28f382b37267
Brian Gouws - work <briangouws@gmail.com>
2013-11-22 16:04:49 +0000

    Queue autoload

--- app/library/App/QueueTwitterStatus.php
+++ app/library/App/QueueTwitterStatus.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Queues;
 
+use Illuminate;
+
 class QueueTwitterStatus
 {
     public function fire($job, $data)
@@ -8,9 +10,28 @@ class QueueTwitterStatus
 
 //        var_dump('Job ID: '.$job->getJobId());
 //        var_dump($data);
-        \Illuminate\Log::info('This is some useful information about job: ' . $job->getJobId());
+        Log::info('This is some useful information about job: ' . $job->getJobId());
 //        sleep(60);
         // NB: be sure to release job else it never leaves
         $job->delete();
     }
+
+    function namespaceExists($namespace)
+    {
+        $namespaces=array();
+        foreach(get_declared_classes() as $name) {
+            if(preg_match_all("@[^\\\]+(?=\\\)@iU", $name, $matches)) {
+                $matches = $matches[0];
+                $parent =&$namespaces;
+                while(count($matches)) {
+                    $match = array_shift($matches);
+                    if(!isset($parent[$match]) && count($matches))
+                        $parent[$match] = array();
+                    $parent =&$parent[$match];
+
+                }
+            }
+        }
+    }
+
 }
