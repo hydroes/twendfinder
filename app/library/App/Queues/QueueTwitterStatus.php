f10b8eb31a8278bb39b91c28c84d82172b33b847
Brian Gouws - work <briangouws@gmail.com>
2013-11-22 13:52:31 +0000

    regenerated autoload

--- app/library/App/Queues/QueueTwitterStatus.php
+++ app/library/App/Queues/QueueTwitterStatus.php
@@ -8,6 +8,7 @@ class QueueTwitterStatus
 
 //        var_dump('Job ID: '.$job->getJobId());
 //        var_dump($data);
+        Log::info('This is some useful information.');
         sleep(120);
         // NB: be sure to release job else it never leaves
         $job->delete();
