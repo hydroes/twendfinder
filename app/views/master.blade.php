48068039cebdee84b5d9853b4d2b198b536851ee
Brian Gouws - work <briangouws@gmail.com>
2013-12-09 13:04:01 +0000

    added autobahn js

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -3,10 +3,11 @@
   <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reputable</title>
+    <title>TwendFinder</title>
   </head>
   <body>
     @yield('content')
     <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="http://autobahn.s3.amazonaws.com/js/autobahn.min.js"></script>
   </body>
 </html>
