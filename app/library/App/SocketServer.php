38e06e8f23edaf4b7ef284b1c1cf5588e01c797f
Brian Gouws - work <briangouws@gmail.com>
2013-12-13 16:14:02 +0000

    adding console messages for debugging sockets

--- app/library/App/SocketServer.php
+++ app/library/App/SocketServer.php
@@ -3,41 +3,61 @@
 use Ratchet\ConnectionInterface;
 use Ratchet\Wamp\WampServerInterface;
 
-class Pusher implements WampServerInterface {
-    public function onSubscribe(ConnectionInterface $conn, $topic) {
+class Pusher implements WampServerInterface
+{
+    protected $_clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        echo 'client connected';
+        $this->clients->attach($conn);
+    }
+
+    public function onSubscribe(ConnectionInterface $conn, $topic)
+    {
         echo 'onSubscribe';
         // When a visitor subscribes to a topic link the Topic object in a  lookup array
         if (!array_key_exists($topic->getId(), $this->subscribedTopics)) {
             $this->subscribedTopics[$topic->getId()] = $topic;
         }
     }
-    public function onUnSubscribe(ConnectionInterface $conn, $topic) {
+    public function onUnSubscribe(ConnectionInterface $conn, $topic)
+    {
+        echo "onUnSubscribe\n";
     }
-    public function onOpen(ConnectionInterface $conn) {
-        echo 'client connected';
+    public function onClose(ConnectionInterface $conn)
+    {
+        echo "Connection {$conn->resourceId} has disconnected\n";
+        $this->clients->detach($conn);
+
     }
-    public function onClose(ConnectionInterface $conn) {
-        echo 'onClose';
-    }
-    public function onCall(ConnectionInterface $conn, $id, $topic, array $params) {
-        echo 'onCall';
+    public function onCall(ConnectionInterface $conn, $id, $topic, array $params)
+    {
+        echo "onCall\n";
         // In this application if clients send data it's because the user hacked around in console
         $conn->callError($id, $topic, 'You are not allowed to make calls')->close();
     }
-    public function onPublish(ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible) {
+    public function onPublish(ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible)
+    {
         echo 'onPublish';
         // In this application if clients send data it's because the user hacked around in console
         $conn->close();
     }
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo 'onError';
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
         $conn->close();
     }
 
     /**
      * @param string JSON'ified string we'll receive from ZeroMQ
      */
-    public function onBlogEntry($entry) {
+    public function onBlogEntry($entry)
+    {
         echo 'onBlogEntry';
 //        $entryData = json_decode($entry, true);
 
