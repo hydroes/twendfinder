a467277330d962f61e74be3f5733a0832d6e4fd1
Brian Gouws - work <briangouws@gmail.com>
2013-12-13 16:21:32 +0000

    adding console messages for debugging sockets

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -9,5 +9,61 @@
     @yield('content')
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="http://autobahn.s3.amazonaws.com/js/autobahn.min.js"></script>
+
+    <script>
+        // WAMP session object
+        var sess;
+        var wsuri = "ws://sockets.twendfinder.com:8080";
+
+        window.onload = function() {
+
+           // connect to WAMP server
+           ab.connect(wsuri,
+
+              // WAMP session was established
+              function (session) {
+
+                 sess = session;
+                 console.log("Connected to " + wsuri);
+
+                 // subscribe to topic, providing an event handler
+                 sess.subscribe("http://example.com/simple", onEvent);
+              },
+
+              // WAMP session is gone
+              function (code, reason) {
+
+                 sess = null;
+                 console.log("Connection lost (" + reason + ")");
+              }
+           );
+        };
+
+        function onEvent(topic, event) {
+           console.log(topic);
+           console.log(event);
+        }
+
+        function publishEvent()
+        {
+           sess.publish("http://example.com/simple", {a: "foo", b: "bar", c: 23});
+        }
+
+        function callProcedure() {
+           // issue an RPC, returns promise object
+           sess.call("http://example.com/simple/calc#add", 23, 7).then(
+
+              // RPC success callback
+              function (res) {
+                 console.log("got result: " + res);
+              },
+
+              // RPC error callback
+              function (error, desc) {
+                 console.log("error: " + desc);
+              }
+           );
+        }
+    </script>
   </body>
 </html>
