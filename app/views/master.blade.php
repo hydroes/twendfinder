6f86bdcf943b73ba5d3fb5504b07f672d76492bc
Brian Gouws - work <briangouws@gmail.com>
2014-01-03 15:14:59 +0000

    socket dev

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -8,62 +8,15 @@
   <body>
     @yield('content')
     <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="http://autobahn.s3.amazonaws.com/js/autobahn.min.js"></script>
 
+    <script src="/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js"></script>
     <script>
-        // WAMP session object
-        var sess;
-        var wsuri = "ws://sockets.twendfinder.com:443";
+    socket = io.connect('http://192.241.120.110:443');
+    socket.on('news', function (data) {
+     console.log(data);
+     socket.emit('my other event', { my: 'data' });
+    });
+  </script>
 
-        window.onload = function() {
-
-           // connect to WAMP server
-           ab.connect(wsuri,
-
-              // WAMP session was established
-              function (session) {
-
-                 sess = session;
-                 console.log("Connected to " + wsuri);
-
-                 // subscribe to topic, providing an event handler http://example.com/simple
-                 sess.subscribe("ws://sockets.twendfinder.com:443", onEvent);
-              },
-
-              // WAMP session is gone
-              function (code, reason) {
-
-                 sess = null;
-                 console.log("Connection lost (" + reason + ")");
-              }
-           );
-        };
-
-        function onEvent(topic, event) {
-           console.log(topic);
-           console.log(event);
-        }
-
-        function publishEvent()
-        {
-           sess.publish("ws://sockets.twendfinder.com:443", {a: "foo", b: "bar", c: 23});
-        }
-
-        function callProcedure() {
-           // issue an RPC, returns promise object
-           sess.call("ws://sockets.twendfinder.com:443", 23, 7).then(
-
-              // RPC success callback
-              function (res) {
-                 console.log("got result: " + res);
-              },
-
-              // RPC error callback
-              function (error, desc) {
-                 console.log("error: " + desc);
-              }
-           );
-        }
-    </script>
   </body>
 </html>
