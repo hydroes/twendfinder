de0840bac829e91823631d5b2a3bc00ae2b3a708
Brian Gouws <briangouws@gmail.com>
2014-02-08 13:43:48 +0000

    Minor template changes

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html lang="en-GB">
+<html lang="en">
   <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
     <title>TwendFinder</title>
