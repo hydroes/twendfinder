2adbfb4a6ba65199c017e4a6918d9a8c3a811816
Brian Gouws - work <briangouws@gmail.com>
2014-03-24 17:35:17 +0000

    Updated composer.json to reflect project properties#

---
+++ app/library/App/StatsCounter.php
@@ -0,0 +1,21 @@
+<?php
+
+class statsCounter
+{
+    /**
+     * Amount of time in minutes to keep data cached for
+     *
+     * @var integer
+     */
+    public $cache_expiry = 10080;
+
+    /**
+     * Ana
+     * @param array $status
+     * @param array $keywords
+     */
+    public static function analyse(array $status, array $keywords)
+    {
+
+    }
+}
