7f3ab6abff316d93835ceda0ec6519e9c0cce167
Brian Gouws - work <hydroes@gnail.com>
2014-04-03 16:24:55 +0100

    Now counts all occurences of a keyword in a string

--- app/library/App/StatsAnalyser.php
+++ app/library/App/StatsAnalyser.php
@@ -26,8 +26,6 @@ class statsAnalyser
         $ttl_status_key = $key_prefix . "_total";
         $this->incrementCounter($ttl_status_key);
 
-        $multi_keyword_count = 0;
-
         // count individual keywords in statuses
         foreach ($keywords as $keyword)
         {
@@ -36,17 +34,33 @@ class statsAnalyser
                 $keyname = $key_prefix . "_{$keyword}";
                 $this->incrementCounter($keyname);
 
-                $multi_keyword_count++;
             }
         }
 
-        // some statuses can contain multiple keywords, count this amount
-        if ($multi_keyword_count > 1)
+    }
+
+    /**
+     * Recursivly iterates through a string and returns how many times a keyword
+     * occurs in the string.
+     *
+     * @param string $string String to search keywords
+     * @param string $keyword Keyword to look for in string
+     * @param int $offset Last offset where keyword was found in string
+     * @param int $keyword_count Number of keywords found
+     * @return integer
+     */
+    protected function _findKeywords($string, $keyword, $offset = 0, &$keyword_count = 0)
+    {
+        if (stripos($string, $keyword, $offset) !== false)
         {
-            $multi_key_name = $key_prefix . "_multi";
-            $this->incrementCounter($multi_key_name);
+            $keyword_count++;
+
+            $offset = (int)$offset + strlen($keyword);
+
+            $this->_findKeywords($string, $keyword, $offset, $keyword_count);
         }
 
+        return $keyword_count;
     }
 
     /**
@@ -54,20 +68,26 @@ class statsAnalyser
      * @param string $key Name of counter increment
      * @return void
      */
-    public function incrementCounter($key)
+    public function incrementCounter($key, $increment_amount = 0)
     {
+        // do nothing if nothing to increment
+        if ((int)$increment_amount === 0)
+        {
+            return;
+        }
+
         $current_count = Cache::get($key, 0);
 
-        $current_count++;
+        $new_count = $current_count + $increment_amount;
 
         // count statuses per minute
         if ($current_count === 0)
         {
-            Cache::add($key, $current_count, $this->cache_expiry);
+            Cache::add($key, $new_count, $this->cache_expiry);
         }
         else
         {
-            Cache::put($key, $current_count, $this->cache_expiry);
+            Cache::put($key, $new_count, $this->cache_expiry);
         }
     }
 }
