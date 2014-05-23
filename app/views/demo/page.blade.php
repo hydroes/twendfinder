869fc2fb5a14137db5c29957c027eafd4f16b4b1
hydroes <hydroes@gmail.com>
2014-05-23 23:30:10 +0100

    refactoring js to angular

--- app/views/demo/page.blade.php
+++ app/views/demo/page.blade.php
@@ -1,6 +1,29 @@
 @extends('master')
 
 @section('content')
+
+<script>
+
+socket = io.connect('http://sockets.twendfinder.com:443');
+
+// My controller
+var twendfinderApp = angular.module('twendfinderApp', []);
+
+twendfinderApp.controller('TweetsCtrl', function ($scope) 
+{
+  
+  $scope.statuses = [];
+  
+  socket.on('tweet', function(data) 
+  {
+      var status = JSON && JSON.parse(data) || $.parseJSON(data);
+      $scope.statuses.push(status);
+  };
+  
+});
+
+</script>
+
 <h1>Welcome!</h1>
 <h3>Below is a live feed of the worlds tweets about love, hate, and other emotions</h3>
 <p>Tweets counted so far: <span id="counter"></span></p>
@@ -14,30 +37,42 @@
 
 <button type="button" class="btn btn-primary" id="feed-flow" data-pause-flow="true">Pause feed</button>
 
-<table id="tweets" class="table table-hover">
-
+<table id="tweets" class="table table-hover" ng-controller="TweetsCtrl">
+    <tr ng-repeat="status in statuses">
+        <td>
+            <img src="'{{status.profile_pic}}'" class="img-rounded" />
+        </td>
+        <td>
+            <div>
+                <a href="https://twitter.com/{{status.screen_name}}" target="_blank">{{status.screen_name}}</a>
+            </div>
+            <div>{{status.text}}</div>
+        </td>
+    </tr>
 </table>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="http://sockets.twendfinder.com/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js"></script>
 <script>
+    
+
 /**
  * TODO: This must be rewritten in angular
  */
 
-socket = io.connect('http://sockets.twendfinder.com:443');
+//socket = io.connect('http://sockets.twendfinder.com:443');
 
 socket.on('init', function()
 {
     socket.emit('get-current-stats-data');
 });
 
-socket.on('tweet', function(data) {
-    var status = JSON && JSON.parse(data) || $.parseJSON(data);
-//    console.log(status)
-    $('#tweets').prepend('<tr><td><img src="'+status.profile_pic+'" class="img-rounded" /></td><td><div><a href="https://twitter.com/'+status.screen_name+'" target="_blank">'+status.screen_name+'</a></div><div>'+status.text+'</div></td></tr>');
-
-});
+//socket.on('tweet', function(data) {
+//    var status = JSON && JSON.parse(data) || $.parseJSON(data);
+////    console.log(status)
+//    $('#tweets').prepend('<tr><td><img src="'+status.profile_pic+'" class="img-rounded" /></td><td><div><a href="https://twitter.com/'+status.screen_name+'" target="_blank">'+status.screen_name+'</a></div><div>'+status.text+'</div></td></tr>');
+//
+//});
 
 socket.on('tweetCount', function(data)
 {
