8d2eca616ed6c06dd3bf8706a114503871368c8b
hydroes <hydroes@gmail.com>
2014-05-23 23:04:26 +0100

    cleaning up formatting

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en" ng-app>
   <head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular.min.js"></script>
     <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <title>TwendFinder</title>
