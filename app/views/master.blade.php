0209beefbf2da294b965dbee17310dd8721401bd
Brian Gouws <brian.gouws@ottilus.com>
2014-09-30 17:56:05 +0100

    Performance enhancements and new socket.io service

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -14,8 +14,8 @@
     <script src="https://www.google.com/jsapi"></script>
     <script src="/js/services.js"></script>
     <script src="/js/controllers.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="http://sockets.twendfinder.com/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js"></script>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
