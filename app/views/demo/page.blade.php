90400ab8adb2e3aa3149ce4336c3b56981c57388
hydroes <hydroes@gmail.com>
2014-10-29 21:52:19 +0000

    fixing stats

--- app/views/demo/page.blade.php
+++ app/views/demo/page.blade.php
@@ -7,9 +7,9 @@
 
     <p ng-controller="StatsCtrl" ng-init="initialize()">
         Tweets counted in the last:
-        <span class="label label-info active">Minute <span class="badge badge-important">{{cur_min}}</span></span>
-        <span class="label label-info active">Hour <span class="badge badge-important" id="cur_hour">0</span></span>
-        <span class="label label-info active">24 hours <span class="badge badge-important" id="cur_day">0</span></span>
+        <span class="label label-info active">Minute <span class="badge badge-important">@{{cur_min}}</span></span>
+        <span class="label label-info active">Hour <span class="badge badge-important">@{{cur_hour}}</span></span>
+        <span class="label label-info active">24 hours <span class="badge badge-important">@{{cur_day}}</span></span>
     </p>
 
     <div ng-controller="TweetsCtrl">
