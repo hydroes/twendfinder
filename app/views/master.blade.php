d29f282548f0ca2c5793e7eea0fae323a2211fc2
hydroes <hydroes@gmail.com>
2014-10-30 20:51:24 +0000

    Added ang sanitize

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -4,13 +4,16 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="bower_components/dist/css/bootstrap.min.css">
     <title>TwendFinder</title>
   </head>
   <body>
     @yield('content')
     
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
+    <script src="bower_components/angular/angular.min.js"></script>
+    <script src="bower_components/angular-sanitize/angular-sanitize.min"></script>
+    
+    
     <script src="https://www.google.com/jsapi"></script>
     <script src="/js/main.js"></script>
     <script src="/js/controllers.js"></script>
