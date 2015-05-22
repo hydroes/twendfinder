f8b926ed1bb65eeb70ec1523572e6060d0f66536
Brian Gouws <brian.gouws@ottilus.com>
2015-05-22 11:44:45 +0100

    Fixed ref err

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 angular.module('twendfinderApp', ['ngSanitize', 'socket-io']);
 
 // define routes
-app.config(['$routeProvider', function($routeProvider){  
+angular.module('twendfinderApp').config(['$routeProvider', function($routeProvider){  
 	$routeProvider
 	.when('/', {
 		templateUrl:'templates/homepage.html',
